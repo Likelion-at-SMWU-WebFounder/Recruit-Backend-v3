58a87d03cd7f1db9cd4baabbc7ee278f7dbfe87b
y11n <samtori3849@gmail.com>
2026-01-05 21:42:03 +0900

    feat: 지원서 제출 시 mailStatus, googleDocsStatus필드 수정
    - 지원서 제출 직후는 'PENDING' 상태
    - 메일 전송 또는 구글 독스 업로드 기능이 성공하면 'SUCCESS' 상태, 실패하면 'FAIL' 상태로 변경

--- src/main/java/com/smlikelion/webfounder/Recruit/event/RecruitmentEventListener.java
+++ src/main/java/com/smlikelion/webfounder/Recruit/event/RecruitmentEventListener.java
@@ -1,13 +1,16 @@
 package com.smlikelion.webfounder.Recruit.event;
 
+import com.smlikelion.webfounder.Recruit.Entity.SendStatus;
 import com.smlikelion.webfounder.Recruit.Service.MailService;
 import com.smlikelion.webfounder.Recruit.Service.RecruitService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.event.TransactionPhase;
 import org.springframework.transaction.event.TransactionalEventListener;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class RecruitmentEventListener {
@@ -18,17 +21,33 @@ public class RecruitmentEventListener {
     @Async
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     public void handleRecruitmentAppliedEvent(RecruitmentAppliedEvent event){
-        recruitService.uploadToGoogleDocs(
-                event.getDocumentId(),
-                event.getApplicationId(),
-                event.getRequest()
-        );
+        try {
+            recruitService.uploadToGoogleDocs(
+                    event.getDocumentId(),
+                    event.getApplicationId(),
+                    event.getRequest()
+            );
+            // 상태를 SUCCESS로 변경
+            recruitService.updateStatus("GOOGLE_DOCS", event.getApplicationId(), SendStatus.SUCCESS);
+        } catch (Exception e) {
+            recruitService.updateStatus("GOOGLE_DOCS", event.getApplicationId(), SendStatus.FAIL);
+            log.error("구글 독스 업로드 실패 - joinerId:{}, 원인:{}", event.getApplicationId(), e.getMessage(), e);
+        }
+
     }
 
     // 2. 메일 전송
     @Async
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     public void handleMailSend(RecruitmentAppliedEvent event) {
-        mailService.sendApplyStatusMail(event.getEmail());
+        try {
+            mailService.sendApplyStatusMail(event.getEmail());
+            // 상태를 SUCCESS로 변경
+            recruitService.updateStatus("MAIL", event.getApplicationId(), SendStatus.SUCCESS);
+        } catch (Exception e) {
+            // 상태를 FAIL로 변경
+            recruitService.updateStatus("MAIL", event.getApplicationId(), SendStatus.FAIL);
+            log.error("메일 전송 실패 - joinerId: {}, 원인:{}", event.getApplicationId(), e.getMessage(), e);
+        }
     }
 }
