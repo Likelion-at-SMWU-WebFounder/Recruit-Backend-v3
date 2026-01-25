82441ae4ff03d6eddde7419a4011a3216bfafe72
Chaemin Yu <chaemin.u@gmail.com>
2026-01-25 23:42:27 +0900

    fix: Google Docs Configuration credentials 경로 수정

--- src/main/java/com/smlikelion/webfounder/global/config/GoogleDocsConfig.java
+++ src/main/java/com/smlikelion/webfounder/global/config/GoogleDocsConfig.java
@@ -1,5 +1,8 @@
 package com.smlikelion.webfounder.global.config;
 
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.HttpRequestInitializer;
+import com.google.api.client.json.gson.GsonFactory;
 import com.google.api.services.docs.v1.Docs;
 import com.google.api.services.docs.v1.DocsScopes;
 import com.google.api.services.docs.v1.model.*;
@@ -13,7 +16,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 
-import java.io.IOException;
+import java.io.*;
+import java.security.GeneralSecurityException;
 import java.util.Collections;
 
 @Slf4j
@@ -21,17 +25,38 @@ import java.util.Collections;
 public class GoogleDocsConfig {
 
     @Bean
-    public Docs docsService() throws IOException {
-        GoogleCredentials credentials = GoogleCredentials.fromStream(
-                        new java.io.FileInputStream("/app/credentials.json")
-                )
-                .createScoped(Collections.singleton(DocsScopes.DOCUMENTS));
+    public Docs docsService() throws IOException, GeneralSecurityException {
+        InputStream credentialsStream = getCredentialsStream();
+
+        GoogleCredentials credentials = GoogleCredentials
+                .fromStream(credentialsStream)
+                .createScoped(Collections.singletonList(DocsScopes.DOCUMENTS));
+
+        HttpRequestInitializer requestInitializer = new HttpCredentialsAdapter(credentials);
 
         return new Docs.Builder(
-                new NetHttpTransport(),
-                JacksonFactory.getDefaultInstance(),
-                new HttpCredentialsAdapter(credentials))
-                .setApplicationName("Recruitment System")
+                GoogleNetHttpTransport.newTrustedTransport(),
+                GsonFactory.getDefaultInstance(),
+                requestInitializer)
+                .setApplicationName("Webfounder")
                 .build();
+    }
+
+    private InputStream getCredentialsStream() throws FileNotFoundException {
+        // 1. EC2 환경: /app/credentials.json 파일 확인
+        File prodFile = new File("/app/credentials.json");
+        if (prodFile.exists()) {
+            return new FileInputStream(prodFile);
+        }
+
+        // 2. 로컬 환경: classpath의 credentials.json 사용
+        InputStream resourceStream = getClass().getClassLoader()
+                .getResourceAsStream("credentials.json");
+
+        if (resourceStream != null) {
+            return resourceStream;
+        }
+
+        throw new FileNotFoundException("credentials.json 파일을 찾을 수 없습니다.");
     }
 }
